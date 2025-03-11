5fcd6c2804d56d3ae7d5e4f5058263a3b81289f6
dev-rvdl <dev-rvdl@outlook.es>
2025-03-11 17:41:20 -0600

    Now we have added a class to stream logs, this is the only responsability of this class

---
+++ Single Responsibility Principle/InvoiceExercise/invoiceApp/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace invoiceApp
+{
+	public class Logger
+	{
+		public Logger()
+		{
+		}
+
+		public void Info(string info)
+		{
+			// here we need to write the Code for info information into the ErrorLog text file
+		}
+		public void Debug(string info)
+		{
+			// here we need to write the Code for Debug information into the ErrorLog text file
+		}
+		public void Error(string message, Exception ex)
+		{
+			// here we need to write the Code for Error information into the ErrorLog text file
+		}
+	}
+}
